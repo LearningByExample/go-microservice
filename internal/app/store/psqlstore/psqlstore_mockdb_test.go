24af900d083fed69935fdfa30e23ff1edeb3e1a4
Estefania <estefania.castro.vizoso@gmail.com>
2020-03-25 22:40:26 +0100

    get pet tests with mock, working on #8

--- internal/app/store/psqlstore/psqlstore_mockdb_test.go
+++ internal/app/store/psqlstore/psqlstore_mockdb_test.go
@@ -23,8 +23,12 @@
 package psqlstore
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/LearningByExample/go-microservice/internal/app/data"
+	"github.com/LearningByExample/go-microservice/internal/app/store"
+	"reflect"
 	"testing"
 )
 
@@ -72,7 +76,7 @@ func TestPosgreSQLPetStore_AddPet_QueryFails(t *testing.T) {
 	defer ps.Close()
 
 	errorWanted := fmt.Errorf("error in query")
-	var sqlinsert = "INSERT INTO pets .* RETURNING id;"
+	sqlinsert := "INSERT INTO pets .* RETURNING id;"
 	mock.ExpectBegin()
 	mock.ExpectQuery(sqlinsert).WithArgs("name", "race", "mod").WillReturnError(errorWanted)
 
@@ -84,7 +88,7 @@ func TestPosgreSQLPetStore_AddPet_CommitFails(t *testing.T) {
 	defer ps.Close()
 
 	errorWanted := fmt.Errorf("error in tx commit")
-	var sqlinsert = "INSERT INTO pets .* RETURNING id;"
+	sqlinsert := "INSERT INTO pets .* RETURNING id;"
 	mock.ExpectBegin()
 	mock.ExpectQuery(sqlinsert).WithArgs("name", "race", "mod").WillReturnRows(mock.NewRows([]string{"id"}).AddRow(1))
 	mock.ExpectCommit().WillReturnError(errorWanted)
@@ -97,7 +101,7 @@ func TestPosgreSQLPetStore_AddPet_RollbackFails(t *testing.T) {
 	defer ps.Close()
 
 	errorWanted := fmt.Errorf("error in tx query")
-	var sqlinsert = "INSERT INTO pets .* RETURNING id;"
+	sqlinsert := "INSERT INTO pets .* RETURNING id;"
 	mock.ExpectBegin()
 	mock.ExpectQuery(sqlinsert).WithArgs("name", "race", "mod").WillReturnError(errorWanted)
 	mock.ExpectRollback().WillReturnError(fmt.Errorf("error in tx rollback"))
@@ -109,7 +113,7 @@ func TestPosgreSQLPetStore_AddPet(t *testing.T) {
 	ps, mock := initDBMock(t)
 	defer ps.Close()
 
-	var sqlinsert = "INSERT INTO pets .* RETURNING id;"
+	sqlinsert := "INSERT INTO pets .* RETURNING id;"
 	mock.ExpectBegin()
 	mock.ExpectQuery(sqlinsert).WithArgs("name", "race", "mod").WillReturnRows(mock.NewRows([]string{"id"}).AddRow(1))
 	mock.ExpectCommit()
@@ -119,6 +123,66 @@ func TestPosgreSQLPetStore_AddPet(t *testing.T) {
 
 	if wanted != got {
 		t.Fatalf("Wrong pet id, wanted  %d, got  %d", wanted, got)
+	}
+	if err != nil {
+		t.Fatalf("Got an unexpected error %q", err)
+	}
+}
+
+func getPetResponseWithError(ps *posgreSQLPetStore, t *testing.T, errorWanted error) {
+	petGot, errorGot := ps.GetPet(1)
+
+	petWanted := data.Pet{}
+	if !reflect.DeepEqual(petGot, petWanted) {
+		t.Fatalf("error, wanted pet %v, got pet %v", petGot, petWanted)
+	}
+
+	if errorWanted != errorGot {
+		t.Fatalf("error wanted %q, error got %q", errorWanted, errorGot)
+	}
+}
+
+func TestPosgreSQLPetStore_GetPet_QueryFails(t *testing.T) {
+	ps, mock := initDBMock(t)
+	defer ps.Close()
+
+	errorWanted := fmt.Errorf("error in tx query")
+	sqlselect := "SELECT id, name, race, mod FROM pets WHERE id = .*"
+	mock.ExpectQuery(sqlselect).WithArgs(1).WillReturnError(errorWanted)
+
+	getPetResponseWithError(ps, t, errorWanted)
+}
+
+func TestPosgreSQLPetStore_GetPet_QueryNoRows(t *testing.T) {
+	ps, mock := initDBMock(t)
+	defer ps.Close()
+
+	sqlselect := "SELECT id, name, race, mod FROM pets WHERE id = .*"
+	mock.ExpectQuery(sqlselect).WithArgs(1).WillReturnError(sql.ErrNoRows)
+
+	errorWanted := store.PetNotFound
+	getPetResponseWithError(ps, t, errorWanted)
+}
+
+func TestPosgreSQLPetStore_GetPet(t *testing.T) {
+	ps, mock := initDBMock(t)
+	defer ps.Close()
+
+	wanted := data.Pet{
+		Id:   1,
+		Name: "fuffly",
+		Race: "dog",
+		Mod:  "happy",
+	}
+
+	sqlselect := "SELECT id, name, race, mod FROM pets WHERE id = .*"
+	var id int64 = 1
+	rows := mock.NewRows([]string{"id", "name", "race", "mod"}).AddRow(id, wanted.Name, wanted.Race, wanted.Mod)
+	mock.ExpectQuery(sqlselect).WithArgs(1).WillReturnRows(rows)
+
+	got, err := ps.GetPet(1)
+	if !reflect.DeepEqual(got, wanted) {
+		t.Fatalf("error, wanted pet %v, got pet %v", got, wanted)
 	}
 	if err != nil {
 		t.Fatalf("Got an unexpected error %q", err)
